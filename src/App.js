68c5b8e76feeee824311cf9d8be90b9bba220463
Helen Dimitriou <heldim@outlook.com>
2025-10-09 23:11:24 +0300

    started CategoriesBar component

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <SignUpBanner />
-      {/* <CategoriesBar /> */}
+      <CategoriesBar />
       <main>
         {/* <TopContent /> */}
         {/* <NewArrivals /> */}
--- src/components/CategoriesBar.jsx
+++ src/components/CategoriesBar.jsx
@@ -1,10 +1,17 @@
+import React from "react";
+
 const CategoriesBar = () => {
   return (
-    <>
-      <header>
-        <nav>Categories</nav>
-      </header>
-    </>
+    <header className="flex">
+      <nav>
+        <ul className="flex">
+          <li> Shop </li>
+          <li> On Sale </li>
+          <li> New Arrivals </li>
+          <li> Brands</li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
