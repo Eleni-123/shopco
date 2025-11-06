7433d6d040cd1259aac3cccc1f3cbfa1f7a3a4d0
Helen Dimitriou <heldim@outlook.com>
2025-11-06 22:17:19 +0200

    vite pnpm project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import SignUpBanner from "./components/SignUpBanner";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <SignUpBanner />
+      {/* Το υπόλοιπο περιεχόμενο της εφαρμογής */}
+    </div>
+  );
+}
+
+export default App;
